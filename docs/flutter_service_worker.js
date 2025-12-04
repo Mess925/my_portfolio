8dbbf0f71b99afbf02b24b83714ff2868959f3ca
Han Min Thant <hanminthant222@gmail.com>
2025-12-05 00:15:06 +0800

    Fix assets

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "45360eb41fc2f78975d2ad44d817daaf",
+const RESOURCES = {"flutter_bootstrap.js": "bfa73fa70c928d83e637b3fc7ab08f6b",
 "version.json": "009c9e65172e010890f7f65fde438006",
 "index.html": "bcf79a10ec495179f1fd7709e5f1cf5d",
 "/": "bcf79a10ec495179f1fd7709e5f1cf5d",
